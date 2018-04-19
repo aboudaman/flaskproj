0cb21532f6569459531ebc8a760b739bfd8dda00
aboudaman <adaman2000@gmail.com>
2018-04-18 20:16:46 -0400

    added crud functionality

---
+++ stringtest.py
@@ -0,0 +1,6 @@
+url = "/restaurants/1/edit"
+
+urlSplit = url.split("/")
+
+for i,val in enumerate(urlSplit):
+    print i, val
--- webserver.py
+++ webserver.py
@@ -1,10 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+
+# import CRUD Operations from Lesson 1
+from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Restaurant, MenuItem, Base
-from sqlalchemy.ext.declarative import declarative_base
-
 
 # Create session and connect to DB
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -12,93 +12,106 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            # Objective 3 Step 2 - Create /restarants/new page
+            if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
                 self.wfile.write(output)
-                print output
                 return
 
-            if self.path.endswith("/restaurants"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-
-                # Get all restaurants
-                restaurants = session.query(Restaurant).all()
-                
-                rest = ""
-
-
-                rest += "<html><body>"
-                rest += "<h1>Restaurant Listing!</h1>"
-                # List all restaurant names
-                for restaurant in restaurants:
-                    rest += restaurant.name + "<br />"
-                    rest +=  "<a href=#>edit</a>&nbsp;"
-                    rest += "<a href=#>delete</a><br /><br />"
-                   
-                    
-                rest += "</body></html>"
-
-                self.wfile.write(rest)
-                return        
-
-            if self.path.endswith("/hola"):
+            if self.path.endswith("/edit"):
+                id = self.path.split("/")[2]
+                print "I am here"
+                print id
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "<h1>Edit a Restaurant</h1>"
+                output += "<p>"
+                # restaurant = session.query(Restaurant).filter_by(id="<id_number>").one()
+                output += "</p>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/%s/edit'>"% id
+                output += "<input name = 'editRestaurantName' type = 'text' placeholder = 'edit restaurant' > "
+                output += "<input type='submit' value='Edit'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+
+
+
+
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                # Objective 3 Step 1 - Create a Link to create a new menu item
+                output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>"
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    # Objective 2 -- Add Edit and Delete Links
+                    output += "<a href ='/restaurants/%s/edit' >Edit </a>" % restaurant.id
+                    output += "</br>"
+                    output += "<a href =' #'> Delete </a>"
+                    output += "</br></br></br>"
+
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
+    # Objective 3 Step 3- Make POST method
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
         except:
             pass
 
+
 def main():
     try:
-        port = 8080
-        server = HTTPServer(('', port), webServerHandler)
-        print "Web Server running on port %s" % port
+        server = HTTPServer(('', 8080), webServerHandler)
+        print 'Web server running... Open localhost:8080/restaurants in your browser'
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print '^C received, shutting down server'
         server.socket.close()
 
 if __name__ == '__main__':
